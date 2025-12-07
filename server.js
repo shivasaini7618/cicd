d58d96f48efeac6cd2a502052fb837b308382e71
shiva saini <shivasaini7618@gmail.com>
2025-12-08 00:10:46 +0530

    sum

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-}
-);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/sum/:a/:b" , (req , res)=>{
+  let a = parseInt(req.params.a);
+  let b = parseInt(req.params.b);
+  let sum =parseInt (a + b);
+  res.send(`The sum of ${a} and ${b} is ${sum}`);
+});
+
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
-}); 
+});
