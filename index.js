05dd365ab725aca8e0e4e87502dafc4257363997
shiva saini <shivasaini7618@gmail.com>
2025-12-23 16:04:12 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+// Middleware to read JSON
+app.use(express.json());
+
+// Home route
+app.get("/", (req, res) => {
+  res.send("Hello World! Node.js app is running 🚀");
+});
+
+// Sample API
+app.get("/api/user", (req, res) => {
+  res.json({
+    name: "Shiva",
+    role: "Backend Developer"
+  });
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
